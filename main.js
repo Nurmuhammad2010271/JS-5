053f4dd1c724963a96d8b735de41b8e800ee4509
Nurmuhammad2010271 <938630762a@gmail.com>
2025-02-11 08:31:38 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let ism = prompt("Ism kriting");
+let Spacedagiid = prompt("Spacedagi id kriting");
+let Spacedagipassword = prompt("Spacedagi password kriting");
+
+let odam = {
+    name: "Nurmuhammad",
+    age: 14,
+    Spacedagiid: 1215058,
+    Spacedagipassword: 89140,
+}
+
+console.log(odam);
